c3d71030651794e1a3d73a37b63240d4d67b220f
Hossein <jadidi.hosein@gmail.com>
2018-04-13 15:32:57 +0800

    Initial Commit

---
+++ TableauVIZ_ASPSingleSignIn/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TableauVIZ_ASPSingleSignIn.MvcApplication" Language="C#" %>
---
+++ TableauVIZ_ASPSingleSignIn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TableauVIZ_ASPSingleSignIn.Startup))]
+namespace TableauVIZ_ASPSingleSignIn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
